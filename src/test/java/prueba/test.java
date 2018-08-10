c4348604df2619d8c4fa663fac9369dff598a9d8
hector <hector@hector>
2018-08-10 09:52:15 -0400

    Commit_03

--- src/test/java/prueba/test.java
+++ src/test/java/prueba/test.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class test {
@@ -14,8 +15,11 @@ public class test {
 	
 	{	
 		WebDriver driver; //Variable global//declaración y creación de instancias de objetos / variables
-		System.setProperty ("webdriver.firefox.marionette", "C: \\ geckodriver.exe");
+		System.setProperty ("webdriver.firefox.marionette", "/home/hector/eclipse-workspace/prueba/driver/geckodriver");
 		driver = new FirefoxDriver (); //vuelvo a cargar el browser
+		//System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		//driver = new ChromeDriver();
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.facebook.com/");
 		
