763ba1d98d7e71ca63224453b895f1bda55a5527
hector <hector@hector>
2018-08-28 13:57:24 -0400

    Commit_13

--- src/test/java/prueba/test.java
+++ src/test/java/prueba/test.java
@@ -3,8 +3,8 @@ package prueba;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class test {
@@ -15,10 +15,10 @@ public class test {
 	
 	{	
 		WebDriver driver; //Variable global//declaración y creación de instancias de objetos / variables
-		//System.setProperty ("webdriver.firefox.marionette", "/home/hector/eclipse-workspace/prueba/driver/geckodriver");
-		//driver = new FirefoxDriver (); //vuelvo a cargar el browser
-		System.setProperty("webdriver.chrome.driver", "/home/hector/eclipse-workspace/prueba/driver/chromedriver");
-		driver = new ChromeDriver();
+		System.setProperty ("webdriver.firefox.marionette", "/home/hector/eclipse-workspace/prueba/driver/geckodriver");
+		driver = new FirefoxDriver (); //vuelvo a cargar el browser
+		//System.setProperty("webdriver.chrome.driver", "/home/hector/eclipse-workspace/prueba/driver/chromedriver");
+		//driver = new ChromeDriver();
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.facebook.com/");
