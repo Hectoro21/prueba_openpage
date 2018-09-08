547047794dcef1af1aa0a844a2f7606708cb1395
hector <hector@hector>
2018-09-08 07:15:24 -0400

    Commit_20

--- src/test/java/prueba/test.java
+++ src/test/java/prueba/test.java
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class test {
@@ -18,10 +17,7 @@ public class test {
 		WebDriver driver; //Variable global//declaración y creación de instancias de objetos / variables
 		//System.setProperty ("webdriver.firefox.marionette", "/home/hector/eclipse-workspace/prueba/driver/geckodriver");
 		//driver = new FirefoxDriver (); //vuelvo a cargar el browser
-		ChromeOptions ChromeOptions = new ChromeOptions (); 
-		ChromeOptions.addArguments ("- headless", "window-size = 1024,768", "--no-sandbox"); 
-		driver = new ChromeDriver (ChromeOptions);
-		System.setProperty("webdriver.chrome.driver", "/home/hector/eclipse-workspace/prueba/driver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		driver = new ChromeDriver();
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
