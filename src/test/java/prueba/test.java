f117a9b7f56f9c7676fdd2007d87ac62bf88ccd6
hector <hector@hector>
2018-09-22 09:25:00 -0400

    Commit_24

--- src/test/java/prueba/test.java
+++ src/test/java/prueba/test.java
@@ -3,9 +3,9 @@ package prueba;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class test {
@@ -16,13 +16,13 @@ public class test {
 	
 	{	
 		WebDriver driver; //Variable global//declaración y creación de instancias de objetos / variables
-		//System.setProperty ("webdriver.firefox.marionette", "/home/hector/eclipse-workspace/prueba/driver/geckodriver");
-		//driver = new FirefoxDriver (); //vuelvo a cargar el browser
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		driver = new ChromeDriver();
+		System.setProperty ("webdriver.firefox.marionette", "/home/hector/eclipse-workspace/prueba/driver/geckodriver");
+		driver = new FirefoxDriver (); //vuelvo a cargar el browser
+		//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		//driver = new ChromeDriver();
 
-		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--no-sandbox");
+		//ChromeOptions chromeOptions = new ChromeOptions();
+		//chromeOptions.addArguments("--no-sandbox");
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.facebook.com/");
